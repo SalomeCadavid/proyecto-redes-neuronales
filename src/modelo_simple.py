a6a75e4ba4ef7715815ae4dd2d045372188614e9
agudelo <agudeloalvarezjuanesteban@gmail.com>
2025-11-30 16:53:33 -0500

    feat: cargar dataset y preparar variables X e y

--- src/modelo_simple.py
+++ src/modelo_simple.py
@@ -1,1 +1,11 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def modelo_ventas_simple():
+    # -------------------- PARTE INTEGRANTE 1 --------------------
+
+    # Cargar dataset
+    df = pd.read_csv('data/dataset_sintetico.csv')
 
