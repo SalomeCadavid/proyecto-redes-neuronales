891caebd05069f0739d14b8b5503e54b59bbed3b
Nicole Sanchez <nicole8715.ma@gmail.com>
2025-12-01 22:15:20 -0500

    feat: agregar funcion para entrenar los modelos simple y multiple

--- src/sistema_integrado.py
+++ src/sistema_integrado.py
@@ -8,3 +8,8 @@ class SistemaIntegrado:
     def __init__(self):
         self.modelo_simple = ModeloSimple()
         self.modelo_multiple = ModeloMultiple()
+        
+    def entrenar_modelos(self, X_train, y_train):
+            self.modelo_simple.entrenar(X_train, y_train)
+            self.modelo_multiple.entrenar(X_train, y_train)
+            print("Entrenamiento completado en ambos modelos.")      
